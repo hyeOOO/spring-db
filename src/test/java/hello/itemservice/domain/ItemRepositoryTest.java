aa74310f385deb462ca66af587d7d12e78f2514c
hyeOOO <hyewon5342@nate.com>
2024-05-14 17:31:45 +0900

    test 작성
    1. test db 서버 분리
    2. 테스트 트랜잭션 사용
    3. 임베디드 모드 사용

--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,19 +5,39 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@Transactional //main에서는 로직이 성공적으로 수행되면 커밋되는데, 테스트에 있으면 스프링은 테스트를 트랜잭션 안에서 실행하고 테스트 종료 시 트랜잭션을 자동으로 롤백해줌
+@SpringBootTest //@SpringBootApplication 위치의 @Import(JdbcTemplateV3Config.class)를 읽어서 가지고 테스트 실행
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository; //인터페이스를 테스트하는게 좋다
+
+/*
+    @Autowired
+    PlatformTransactionManager transactionManager; //트랜잭션 매니저(트랜잭션 동기화도 지원)
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforeEach(){
+        //트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+*/
+
 
     @AfterEach
     void afterEach() {
@@ -25,8 +45,12 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+        //트랜잭션 롤백
+//        transactionManager.rollback(status);
     }
 
+//    @Commit //롤백되지 않고 커밋
+//    @Transactional
     @Test
     void save() {
         //given
