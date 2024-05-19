97a22216713dc496bff3de9d90468adc37675b56
hyeOOO <hyewon5342@nate.com>
2024-05-19 22:58:24 +0900

    QueryDSL 적용

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -20,7 +20,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV3Config.class)
 //@Import(MyBatisConfig.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web") //컴포넌트 스캔 범위 지정
 public class ItemServiceApplication {
 
